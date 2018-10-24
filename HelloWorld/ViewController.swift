a8b6ad554a287913cca74825144855af47b9509b
Akira Kamite <akirakamite@akiranomacbook-puro.local>
2018-10-24 19:38:52 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Akira Kamite on 2018/10/24.
+//  Copyright © 2018年 araki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
